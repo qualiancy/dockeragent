42abcfe86c692bf6c274804fee847fc5d1e2a85c
Jake Luer <jake@alogicalparadox.com>
2013-08-27 17:25:34 +0300

    example: [events] use env vars

--- examples/events.js
+++ examples/events.js
@@ -1,7 +1,15 @@
+var DOCKER_HOST = process.env.DOCKERAGENT_HOST || 'localhost';;;;
+var DOCKER_PORT = parseFloat(process.env.DOCKERAGENT_PORT || 4243);
+
 var docker = require('..');
-var remote = docker.Remote();
+var remote = new docker.Remote;
+
+remote
+.set('host', DOCKER_HOST)
+.set('post', DOCKER_PORT);
 
 var ee = remote.events();
+
 ee.on('status', function(line) {
   console.log('status', line);
 });
