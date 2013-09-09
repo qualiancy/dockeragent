a8e13634cea6ab875af14de6cbf4d016f5498e31
Jake Luer <jake@alogicalparadox.com>
2013-09-08 20:55:14 -0400

    remote: construct with url

--- test/remote.system.js
+++ test/remote.system.js
@@ -3,15 +3,50 @@ var SIMPLE_SCRIPT = [ 'date' ];
 var INFINITE_SCRIPT = [ '/bin/sh', '-c', 'while true; do echo hello universe; sleep 1; done' ]
 
 describe('(system) remote', function () {
-  var remote = new dockeragent.Remote
+  var Remote = dockeragent.Remote;
+  var remote = new Remote;
   remote.set('host', DOCKER_HOST);
   remote.set('port', DOCKER_PORT);
 
-  function addImage (name)  {
-    return function (done) {
-      remote.pull(name, noErr(done));
-    }
-  }
+  describe('construction', function() {
+    describe('Remote()', function() {
+      it('uses default values', function() {
+        var rem = new Remote;
+        rem.get('host').should.equal('localhost');
+        rem.get('port').should.equal(4243);
+        rem.enabled('secure').should.be.false;
+      });
+    });
+
+    describe('Remote("docker.io:5000")', function() {
+      it('uses user provide host/port', function() {
+        var rem = new Remote('docker.io:5000');
+        rem.get('host').should.equal('docker.io');
+        rem.get('port').should.equal(5000);
+        rem.enabled('secure').should.be.false;
+      });
+    });
+
+    describe('Remote("https://docker.io:5000")', function() {
+      it('enables secure', function() {
+        var rem = new Remote('https://docker.io:5000');
+        rem.get('host').should.equal('docker.io');
+        rem.get('port').should.equal(5000);
+        rem.enabled('secure').should.be.true;
+      });
+    });
+
+    describe('Remote("https://user:pass@docker.io:5000")', function() {
+      it('detects user credentals', function() {
+        var rem = new Remote('https://user:pass@docker.io:5000');
+        rem.get('host').should.equal('docker.io');
+        rem.get('port').should.equal(5000);
+        rem.enabled('secure').should.be.true;
+        rem.get('username').should.equal('user');
+        rem.get('password').should.equal('pass');
+      });
+    });
+  });
 
   describe('.version(cb)', function() {
     it('requests the version object', function(done) {
